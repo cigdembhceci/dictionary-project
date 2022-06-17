beb91d889094ad001377ac2cac5aae4e18323061
cigdembhceci <cigdembhceci@hotmail.com>
2022-06-16 22:48:09 -0400

    added form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">Coded by Cigdem B</footer>
     </div>
   );
 }
